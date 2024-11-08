8753943bc788b222165b0f9c227a188ce4b27fca
Cristobal Vasquez <cristobal.vasquez.m@usach.cl>
2024-11-07 22:02:43 -0300

    routing for nginx fixed

--- src/routes.ts
+++ src/routes.ts
@@ -4,80 +4,80 @@ export const Routes = [
     // user rates expert
 {
     method: "get",
-    route: "/api/v1/comments-expert",
+    route: "/comments-expert",
     controller: MainController,
     action: "userRatesExpertAll"
 }, {
     method: "get",
-    route: "/api/v1/comments-expert/:id",
+    route: "/comments-expert/:id",
     controller: MainController,
     action: "userRatesExpertOne"
 }, {
     method: "post",
-    route: "/api/v1/comments-expert",
+    route: "/comments-expert",
     controller: MainController,
     action: "userRatesExpertSave"
 }, {
     method: "patch",
-    route: "/api/v1/comments-expert/:id",
+    route: "/comments-expert/:id",
     controller: MainController,
     action: "userRatesExpertUpdate"
 }, {
     method: "delete",
-    route: "/api/v1/comments-expert/:id",
+    route: "/comments-expert/:id",
     controller: MainController,
     action: "userRatesExpertRemove"
 }, 
     // user rates store
 {
     method: "get",
-    route: "/api/v1/comments-store",
+    route: "/comments-store",
     controller: MainController,
     action: "userRatesStoreAll"
 }, {
     method: "get",
-    route: "/api/v1/comments-store/:id",
+    route: "/comments-store/:id",
     controller: MainController,
     action: "userRatesStoreOne"
 }, {
     method: "post",
-    route: "/api/v1/comments-store",
+    route: "/comments-store",
     controller: MainController,
     action: "userRatesStoreSave"
 }, {
     method: "patch",
-    route: "/api/v1/comments-store/:id",
+    route: "/comments-store/:id",
     controller: MainController,
     action: "userRatesStoreUpdate"
 }, {
     method: "delete",
-    route: "/api/v1/comments-store/:id",
+    route: "/comments-store/:id",
     controller: MainController,
     action: "userRatesStoreRemove"
 // food comments
 }, {
     method: "get",
-    route: "/api/v1/comments-food",
+    route: "/comments-food",
     controller: MainController,
     action: "userCommentsFoodAll"
 }, {
     method: "get",
-    route: "/api/v1/comments-food/:id",
+    route: "/comments-food/:id",
     controller: MainController,
     action: "userCommentsFoodOne"
 }, {
     method: "post",
-    route: "/api/v1/comments-food",
+    route: "/comments-food",
     controller: MainController,
     action: "userCommentsFoodSave"
 }, {
     method: "patch",
-    route: "/api/v1/comments-food/:id",
+    route: "/comments-food/:id",
     controller: MainController,
     action: "userCommentsFoodUpdate"
 }, {
     method: "delete",
-    route: "/api/v1/comments-food/:id",
+    route: "/comments-food/:id",
     controller: MainController,
     action: "userCommentsFoodRemove"
 }]
